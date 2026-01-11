88c619927fdddd6480c2d46e9e64d619e9274c3f
Vũ Ngọc Nam <vungocnam2000nd@gmail.com>
2026-01-11 17:14:44 +0700

    add naive buy service with logging

--- src/main/java/com/NamVu/concurrency_lab/config/DataInitializer.java
+++ src/main/java/com/NamVu/concurrency_lab/config/DataInitializer.java
@@ -3,16 +3,14 @@ package com.NamVu.concurrency_lab.config;
 import com.NamVu.concurrency_lab.domain.Product;
 import com.NamVu.concurrency_lab.repository.ProductRepository;
 import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class DataInitializer {
 
     private final ProductRepository productRepository;
-
-    public DataInitializer(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
 
     @PostConstruct
     public void init() {
---
+++ src/main/java/com/NamVu/concurrency_lab/service/OrderService.java
@@ -0,0 +1,38 @@
+package com.NamVu.concurrency_lab.service;
+
+import com.NamVu.concurrency_lab.domain.Product;
+import com.NamVu.concurrency_lab.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final ProductRepository productRepository;
+
+    // intentionally naive
+    public boolean buy(Long productId) {
+        String thread = Thread.currentThread().getName();
+        log.info("[{}] Start buy", thread);
+
+        Product product = productRepository.findById(productId).orElseThrow();
+
+        log.info("[{}] Current stock = {}", thread, product.getStock());
+
+        if (product.getStock() > 0) {
+            product.decreaseStock();
+
+            log.info("[{}] Stock after decrease = {}", thread, product.getStock());
+
+            productRepository.save(product);
+            log.info("[{}] Buy success", thread);
+            return true;
+        }
+
+        log.warn("[{}] Buy failed - out of stock", thread);
+        return false;
+    }
+}
